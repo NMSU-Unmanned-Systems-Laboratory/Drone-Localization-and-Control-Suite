1c2ac5cf6fbd62de86e484a59cb8ea6cba2d4b83
Nicholas Grijalva <nicholasgrijalva01@gmail.com>
2024-02-07 12:11:17 -0700

    Added requirement to isntall zeroconf for pyparrot

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='Drone_Loc_and_Cont_Suite',
     packages=find_packages(include=['Drone_Loc_and_Cont_Suite']),
-    version='0.4.1',
+    version='0.4.2',
     description='A Library for integrated localization and control using an Optitrack system with various models of UAV quadrotor crafts',
     author='KnickKnaack nicholasgrijalva01@gmail.com',
-    install_requires=['pyparrot', 'djitellopy', 'rospy', 'numpy', 'simple_pid']
+    install_requires=['pyparrot', 'djitellopy', 'rospy', 'numpy', 'simple_pid', 'zeroconf']
 )
